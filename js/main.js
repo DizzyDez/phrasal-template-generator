6fd255ffd71a5d30e824b6be2a8d3fd9e4096ba3
DizzyDez <desc0137@algonquinlive.com>
2016-03-03 21:28:43 -0500

    Found how to prompt my user for the information, but I don't know where to start in placing that information

---
+++ js/main.js
@@ -0,0 +1,21 @@
+var i;
+
+var colour = prompt('A colour (orange, indigo, etc.)');
+
+var aCreature = prompt('A creature (dragon, moth, etc.)');
+
+var anAdjective = prompt('An adjective (beautiful, super, etc.)');
+
+var aVerb = prompt('A past tense verb (ran, burped, etc.)');
+
+var story = prompt('Which story would you like?');
+
+var story = function () {
+  document.write('After snacking on');
+  document.write(anAdjective);
+  document.write('treats, the');
+  document.write(colour);
+  document.write('bellied');
+  document.write(aCreature + aVerb);
+  document.write('for hours.');
+};
